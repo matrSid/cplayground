b45a2bebdfd122327897b96149328e152228d389
matrSid <siddhantj972@gmail.com>
2024-10-18 19:11:00 +0530

    fix

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ apt-get update && apt-get install -y g++
 
 # Install Python dependencies
 pip install -r requirements.txt
+
+# Install Node.js dependencies and build the frontend
+npm install
+npm run build
